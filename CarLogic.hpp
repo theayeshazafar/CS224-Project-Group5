c1e347321df8ef8f7a76fedaa61afba1a6a69cbd
HasanKhan710 <iamhasan710@gmail.com>
2023-12-05 22:23:43 +0500

    Implementing Car Logic class
    Responsible for logic for "user car", that will be driven by player/user. This class is responsible for the collision detection, and drawing of car assets on screen. It creates the required object like user car.

---
+++ CarLogic.hpp
@@ -0,0 +1,36 @@
+#include "PlayerCar.hpp"
+#include <vector>
+#include <list>
+#include <SDL_mixer.h>
+#include <SDL.h>
+using namespace std;
+
+// The Car_logic class is the main class that contains the logic for PlayerCar, that will be driven by player
+// This class is responsible for the collision detection, score calculation, drawing of assets on screen.
+// It creates the required objects like PlayerCar and the Obstacles that moves on the screen .
+class Car_Logic
+{
+    // This is the score for our Game.
+    int scores = 0;
+    PlayerCar playercar;
+    Mix_Music *music = NULL;
+    // Screen dimension constants
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
+
+public:
+    int  getscore();
+    bool passed_obstacle = false;
+    bool checkup = false;
+    bool checkdown = false;
+    bool checkright = false;
+    bool checkleft = false;
+    bool objectcheck = false;
+    void drawObjects();
+    void createObject();
+    void CheckCollision();
+    /*Game class has been made the friend class of car_Logic class so that it can access the members of 
+    CarLogic class.*/
+    friend class Game;
+    ~Car_Logic();
+};
