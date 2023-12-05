47db45e7b43c062d79836022ceb34bc22303587e
HasanKhan710 <iamhasan710@gmail.com>
2023-12-05 23:20:53 +0500

    Creating Unit.hpp
    This file defines the `Unit` class, serving as the parent class for all assets in the game, providing common functionalities like drawing and movement.
    
    Co-Authored-By: theayeshazafar <122561076+theayeshazafar@users.noreply.github.com>

---
+++ Unit.hpp
@@ -0,0 +1,18 @@
+#include <SDL.h>
+#include "drawing.hpp"
+#include <iostream>
+using namespace std;
+#pragma once
+
+//Parent Class for all the assets in the game
+class Unit{             //defining a class named unit as required that is to be inherited by all the assets like cars, Road Barriors, PoliceCar, etc.
+    protected:
+        SDL_Rect srcRect, moverRect; //These values will be inherited by all the assets like cars, Road Barriors, PoliceCar, etc. This would contain initial srcRect, moverRect values.
+        int animation = 0;           //An animation variable that will be inherited by all the assets like cars, Road Barriors, PoliceCar, etc.
+    public:
+        void draw();             //A predefined draw function that will be inherited by all the assets like cars, Road Barriors, PoliceCar, etc to draw objects.
+        virtual void move();     //A virtual move function since each object have different srcRect values for animation, this function cannot be predefined but would be defined individually in each class.
+        friend class Car_Logic;  //making Car_Logic as a friend class since we need the access of the moveRect values in the Unit type list to check collision in Car_Logic class.
+        ~Unit();                    //A virtual destructor for the Unit class.
+        friend class Game;
+};
