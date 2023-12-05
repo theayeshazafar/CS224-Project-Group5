f75b452531a4d786380c912e4e8d20c0f529e05a
HasanKhan710 <iamhasan710@gmail.com>
2023-12-05 23:17:01 +0500

    Game.hpp
    This file defines the main `Game` class, responsible for the game loop, loading screens, and managing different states of the game.
    
    Co-Authored-By: theayeshazafar <122561076+theayeshazafar@users.noreply.github.com>

--- Game.hpp
+++ Game.hpp
@@ -1,25 +1,34 @@
 #include <SDL.h>
 #include <iostream>
 #include <SDL_mixer.h>
-
-// Game Logic Class 
-
+#include "drawing.hpp"
+#include "DifficultyLevel.hpp"
+//This is the main class of the game responsible for the game loop.
+//It loads the main game screen, Game Over screen, the instructions screen, etc.
 class Game
 {
-    const int SCREEN_HEIGHT = 600;
+    //Screen dimension constants
     const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
     //The window we'll be rendering to
     SDL_Window* gWindow = NULL;
-
     //Current displayed texture
     SDL_Texture* gTexture = NULL;
-    
+    //Pointer to Background Music
     Mix_Music* music = NULL;
-
-    public:
-        SDL_Texture Texture_Loading(); 
-        SDL_Window Rendering_of_window();
-        void loadsound();
-        void playsound();
-        void Loading_of_Texture();
+    /* State variable will hold the information in the form of integers for each corresponding screen, 
+    such as Start Game Screen, */ 
+    int state = 0;
+public:
+    //This function will initialize the SDL videosystem, texture, audio, creates window and finally return flag of success of initilazing
+    bool init();  
+    bool loadMedia();
+    bool loadGame();
+    bool loadInstructions();
+    bool loadLevel();
+    bool loadvenues();
+    void close();
+    bool loadendscreen();
+    SDL_Texture* loadTexture( std::string path );
+    void run();
 };
