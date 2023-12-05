a8da3bd50ce32a9e13be9458149912c1d101b5b9
HasanKhan710 <iamhasan710@gmail.com>
2023-12-05 23:20:29 +0500

    Creating PlayerCar,hpp file
    This file defines the `PlayerCar` class, responsible for drawing and controlling the player's car.
    
    Co-Authored-By: theayeshazafar <122561076+theayeshazafar@users.noreply.github.com>

---
+++ PlayerCar.hpp
@@ -0,0 +1,20 @@
+#include<SDL.h>
+
+//This class is responsible for drawing the Player Car on the screen..
+//Car_Logic has been made a friend class of this class 
+
+class PlayerCar
+{
+
+    SDL_Rect srcRect, moverRect;
+
+public:
+    void draw();
+    PlayerCar(); 
+    friend class Car_Logic;
+    ~PlayerCar();
+    void operator++(); //This is the overloaded ++ operator to move the PlayerCar to the right.
+    void operator--(); //This is the overloaded -- operator to move the PlayerCar to the left.
+    void operator+(); //This is the overloaded + operator to move the PlayerCar to the up.
+    void operator-(); //This is the overloaded - operator to move the PlayerCar to the down.
+};
