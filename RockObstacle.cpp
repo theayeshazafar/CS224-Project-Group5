354e09da455236baba6497dfbea9bf425b773058
theayeshazafar <122561076+theayeshazafar@users.noreply.github.com>
2023-12-07 13:45:50 +0500

    Update Rock Obstacle code

---
+++ RockObstacle.cpp
@@ -0,0 +1,31 @@
+#include "RockObstacle.hpp"
+#include "drawing.hpp"
+
+//This function is used to move the rocks on the screen and it is overriden from the Unit class
+void RockObstacle::move()
+{
+    moverRect.y += (4*DifficultyLevel::getSpeed());
+    if (moverRect.y > 650)
+    {
+        moverRect.y = -200;
+    }
+}
+
+//This is the default constructor for the RockObstacle class
+RockObstacle::RockObstacle()
+{
+    srcRect = {139,345,119,78}; 
+    moverRect = {405, -20, 120, 40};
+}
+
+//This is the overloaded constructor for the RockObstacle class which takes in the x and y coordinates of the rocks
+RockObstacle::RockObstacle(int x, int y)
+{      
+    srcRect = {139,345,119,78};    
+    moverRect = {x , y, 120, 40};      
+}
+
+//This is the destructor for the RockObstacle class
+RockObstacle::~RockObstacle()
+{}
+
