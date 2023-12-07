7569a4fc63991012ce4b4de15683472aaf381825
theayeshazafar <122561076+theayeshazafar@users.noreply.github.com>
2023-12-07 14:25:32 +0500

    Writing the code for Red Car same as that of Blue Car
    Co-Authored-By: HasanKhan710 <150110648+HasanKhan710@users.noreply.github.com>
    Co-Authored-By: SyedFahadNadeem. <100086370+11223344SFN@users.noreply.github.com>

---
+++ RedCar.cpp
@@ -0,0 +1,32 @@
+#include "RedCar.hpp"
+#include "drawing.hpp"
+#include "DifficultyLevel.hpp"
+
+//The move function is overriden from the Unit class
+void RedCar::move()
+{
+    moverRect.y += (10*DifficultyLevel::getSpeed());
+    if (moverRect.y > 680)
+    {
+        moverRect.y = -50;
+    }
+}
+
+//This is the default constructor for the RedCar class
+RedCar::RedCar()
+{
+    srcRect = {289,14,93,190}; 
+    moverRect = {540, 0, 60, 140};
+}
+
+//This is the overloaded constructor for the RedCar class which takes in the x and y coordinates of the RedCar
+//and dynamically creates the RedCar on the screen at the specified co-ordinates from the factory class
+RedCar::RedCar(int x, int y)
+{       
+    srcRect = {289,14,93,190};       
+    moverRect = {x , y, 60, 140};      
+}
+
+//This is the destructor for the RedCar class
+RedCar::~RedCar()
+{}
