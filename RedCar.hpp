20111c1824a2fa75a69b222b44b39eda63442f55
theayeshazafar <122561076+theayeshazafar@users.noreply.github.com>
2023-12-07 14:19:51 +0500

    Making the Red Car move function
    Co-Authored-By: SyedFahadNadeem. <100086370+11223344SFN@users.noreply.github.com>
    Co-Authored-By: HasanKhan710 <150110648+HasanKhan710@users.noreply.github.com>

---
+++ RedCar.hpp
@@ -0,0 +1,14 @@
+#include "drawing.hpp"
+#include "Unit.hpp"
+#pragma once
+
+//Class RedCar inherting from Unit class and drawing the RedCar on the screen and moving it through its move function
+//The move function is overriden from the Unit class
+class RedCar : public Unit
+{
+    public:
+        void move();
+        RedCar(); 
+        RedCar(int x, int y);
+        ~RedCar();
+};
