41856f540a9cde95bec825ee2e9d3f6c5d580a70
theayeshazafar <122561076+theayeshazafar@users.noreply.github.com>
2023-12-07 09:22:56 +0500

    Updating Unit.cpp
    Adding the drawing and rendering function + virtual move

---
+++ Unit.cpp
@@ -0,0 +1,16 @@
+#include<SDL.h>
+#include "Unit.hpp"
+
+//This function draws and renders all the objects present in the Unit type list.
+void Unit::draw()
+{
+    SDL_RenderCopy(Drawing::gRenderer, Drawing::assets, &srcRect, &moverRect);
+}
+
+//A virtual move that moves all the objects present in the Unit type list.
+void Unit::move()
+{}
+
+//This is the destructor for the Unit class.
+Unit::~Unit()
+{}
