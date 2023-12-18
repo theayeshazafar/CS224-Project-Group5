6b45feacf5e3b74fb80a7062232aee24e7523b24
11223344SFN <100086370+11223344SFN@users.noreply.github.com>
2023-12-18 10:22:48 -0800

    Update Health.hpp

--- Health.hpp
+++ Health.hpp
@@ -3,12 +3,3 @@
 #include "Unit.hpp"
 #pragma once;
 
-//Health class inherting from Unit class and drawing the Health on the screen.
-// The Health symbols will be an static picture on screen and does not move.
-class Health : public Unit
-{
-    public:
-        Health(); 
-        Health(int x);
-        ~Health();
-};
