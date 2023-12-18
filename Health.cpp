428963bbc1d6ef82dd919e811a69c28935de4e96
11223344SFN <100086370+11223344SFN@users.noreply.github.com>
2023-12-18 10:29:44 -0800

    Worked on Health.cpp file

--- Health.cpp
+++ Health.cpp
@@ -7,3 +7,14 @@ Health :: Health()
     moverRect = {709,0, 30, 30};
 }
 
+//This is the overloaded constructor for the Health class which takes in the x and y coordinates of the Health
+//and dynamically creates the Health on the screen at the specified co-ordinates from the factory class
+Health :: Health(int x)
+{
+    srcRect = {400, 20, 70, 60};
+    moverRect = {x, 0, 30, 30};
+}
+
+//This is the destructor for the Health class
+Health :: ~Health()
+{}
