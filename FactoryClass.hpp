280967c72d3cd39bf597c00d6cabdaebe2f74eef
HasanKhan710 <iamhasan710@gmail.com>
2023-12-19 14:20:37 +0500

    Adding comments to Factory Class

--- FactoryClass.hpp
+++ FactoryClass.hpp
@@ -12,11 +12,19 @@
 #include<SDL.h>
 using namespace std;
 
+/*Polymorphism is done here as it allows us to treat objects of different classes through a common interface. 
+Here the list<Unit *> objects achieves polymorphism by storing pointers to objects 
+of various derived classes in a list, where all these classes inherit from the common base class Unit. And the objects are iteratively
+drawn in CarLogic Class*/
+
+// FactoryClass focuses on the dynamic creation and management of instances of game assets.
+
 /*This class is the core class that creates all objects dynmically and pushes them into a Unit* type list 
 which is used in the GameLogic class.*/
 class FactoryClass : public Unit
 {
-    list<Unit*> objects;
+    list<Unit*> objects; /* helps in encapsulating objects so that they can be iterated for collision and drawing, 
+    achieves runtime dynamic creation */
     friend class Car_Logic;
     friend class Game;
     int number_of_collisions=0;
